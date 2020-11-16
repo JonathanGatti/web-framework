41851f63bdd375e49fac6c08ba8ab2b9df2ea8c4
jonathan gatti <jonathangatti2@gmail.com>
2020-11-16 12:55:11 +0000

    created Evening class to handle events

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,3 @@ import { User } from "./models/User";
 
 const user = new User({ id: 1 });
 
-user.set({ 'age': 9000 })
-user.set({ 'name': 'adsadjak' });
-user.save()
-
-const gino = new User({ name: 'gino', age: 20 });
-gino.save()

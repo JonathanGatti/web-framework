ce7682677e5f2792679e21d9a5a3672e71a64ba0
jonathan gatti <jonathangatti2@gmail.com>
2020-11-17 09:14:00 +0000

    created the model class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,4 @@
-import { AxiosResponse } from "axios";
-import { Attributes } from "./Attributes";
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { Model } from './Model';
 
 export interface UserProps {
   id?: number;
@@ -10,43 +7,6 @@ export interface UserProps {
 } 
 const url = 'http://localhost:3000/users'
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(url);
-  public attributes: Attributes<UserProps>;
-  constructor(attrs: UserProps) { 
-    this.attributes = new Attributes<UserProps>(attrs)
-  };
-
-  get on() {
-    return this.events.on;
-  }
-  get trigger() {
-    return this.events.trigger;
-  }
-  get get() {
-    return this.attributes.get;
-  }
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change')
-  }
-  fetch(): void {
-    const id = this.attributes.get('id');
-    if (typeof id !== 'number') {
-      throw new Error('cannot fetch with no id')
-    }
-    this.sync.fetch(id).then((res: AxiosResponse): void => (
-      this.set(res.data)
-    ))
-  }
-  save(): void {
-    const data = this.attributes.getAll();
-    this.sync.save(data).then((res: AxiosResponse) => {
-      this.trigger('save');
-    })
-      .catch((e) => {
-        console.log(e)
-    })
-  }
+export class User extends Model {
+  
 }

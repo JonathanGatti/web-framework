bc03757c61f66750a2e6e2f25cf42e74734ff21a
jonathan gatti <jonathangatti2@gmail.com>
2020-11-17 14:03:10 +0000

    created View class

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { UserForm } from "./views/UserForm";
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Gino', age: 20 });
-const userForm = new UserForm(document.querySelector('#root'), user);
 
-userForm.render();
+const root = document.querySelector('#root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('root element not found');
+}

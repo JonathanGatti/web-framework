6e8f99a58cc830e59e8688c57276f6808f818ebb
jonathan gatti <jonathangatti2@gmail.com>
2020-11-17 11:20:47 +0000

    created user form element

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { User } from "./models/User";
 
-const collection  = User.buildUserCollection()
+const collection = User.buildUserCollection()
+
 collection.on('change', () => {
   console.log(collection)
 });
